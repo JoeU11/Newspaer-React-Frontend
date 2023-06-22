fb3f8ee6614c4295a16f5d03b82ccd21ad93d5a6
joe <joe.ulica@gmail.com>
2023-06-22 12:30:53 -0400

    turn article titles on index page links to article show page

--- src/components/article.js
+++ src/components/article.js
@@ -2,7 +2,7 @@ export default function Article(article) {
   return (
     <div id="article">
       {article.photos[0] && <img src={article.photos[0].url} style={{ width: 300 }} alt="article" />}
-      <h1>{article.title}</h1>
+      <a href={`/articles/${article.id}`}><h1>{article.title}</h1></a>
       <p> by {article.author}</p>
     </div>
   )
