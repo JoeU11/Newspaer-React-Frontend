56b2fce76ae88f1120e35b35d81d7ef7d504ef35
joe <joe.ulica@gmail.com>
2023-06-22 12:36:00 -0400

    add home link + add basic styling to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,9 @@ export default function Navbar() {
   return (
     <nav>
       <h1>Welcome to the Newspaper App</h1>
+      <div id="nav-links">
+        <a href="/">home</a>
+      </div>
       {/* add nav links here */}
     </nav>
   )
